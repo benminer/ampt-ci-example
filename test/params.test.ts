f85e0278a7fc85d678089415ebf4681154536b89
Ben Miner <bdminer96@gmail.com>
2023-08-18 16:04:05 -0500

    update test to pass

--- test/params.test.ts
+++ test/params.test.ts
@@ -4,6 +4,6 @@ import { describe, expect, it } from "vitest";
 describe("params", () => {
   it("should have url param", async () => {
     const stage = await params("ENV_NAME");
-    expect("wrong stage name").toEqual(stage);
+    expect("ben").toEqual(stage);
   });
 });
